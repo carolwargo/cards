e5b384c3b1ac7e3766d7b8b0b3265979995281df
Carol Wargo <Carolwargo@gmail.com>
2024-05-07 10:28:16 -0400

    added tailwind config

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes }  from 'react-router-dom';
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import HomePage from './HomePage';
+
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/" element={<HomePage />} /> 
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
   );
 }
-
+// Create a component for handling not found routes
+function NotFound() {
+  console.error("Page not found!"); // Log error to console
+  return <h1>404 - Not Found</h1>;
+}
 export default App;
