f7e567beef1317f08e5c24224fe64a31554ce722
Carol Wargo <Carolwargo@gmail.com>
2024-05-10 07:50:16 -0400

    deleted files and working on responsive animation

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React from 'react';
 // import components
 import ImageSlide from './components/ImageSlide';
-//import CardFlip from './components/CardFlip';
 import Card from './components/Card';
-//import Carousel from './components/Carousel';
-//import CardSlider2 from './components/CardSlider2';
-//import CardSlider1 from './components/CardSlider1'; 
 import ProductCard from './components/ProductCard';
 
 const App = () => {
   return (
     <div className='overflow-hidden'>
-      <div className='items-center justify-center p-10 md:container md:mx-auto'>
-        <h1>The following card annimations are done with Tailwind CSS and framer-motion. </h1>
-      </div>
+     
       
       <div>
       <Card />
@@ -23,12 +17,7 @@ const App = () => {
       </div>
      
       <div> <ImageSlide /></div>
-     
   
-     {/*} <CardFlip />*/}
-      {/*<Carousel />*/}
-      {/**<CardSlider2 />*/}
-      {/*<CardSlider1 />*/}
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    
     extend: {
-      backgroundImage: {
-        page: "url('./img/background.png')",
-      },
       fontFamily: {
   sans: ['dmserif', 'DM Serif', 'serif'],
   com: ['conforta', 'Conforta', 'sans-serif']
